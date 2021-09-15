82203416bfe61b1b33d5b66560e159f69f429850
Adinda Nur Aulia Rizki <adindanar12@gmail.com>
2021-09-15 14:22:49 +0700

    Create biodata.php

---
+++ biodata.php
@@ -0,0 +1,24 @@
+<?php
+$nama = "Adinda Nur Aulia Rizki";
+$kelas = "MI20B";
+$Alamat = "Tasikmalaya";?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata</title>
+</head>
+<body>
+	<?php
+	echo "BIODATA";
+	echo "<br>";
+	echo "<br>";
+	echo "Nama : ".$nama;
+	echo "<br>";
+	echo "Kelas : ".$kelas;
+	echo "<br>";
+	echo "Alamat : ".$Alamat;
+
+	?>
+</body>
+</html>
