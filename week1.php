9e4cb3738673ed7b0d5b82907e352e30727a186f
Adinda Nur Aulia Rizki <adindanar12@gmail.com>
2021-09-15 14:22:05 +0700

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,18 @@
+<?php
+$nama = "Adinda Nur Aulia Rizki";?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>First Week - Adinda Nur Aulia Rizki</title>
+</head>
+<body>
+	<?php
+
+	echo "Hello World";
+	echo "<br>"; // Untuk menambahkan spasi atau membuat teks pindah kebawah
+	echo $nama; //(string adalah karakter)($NAMA=NAMA ADALAH VARIABEL)
+	echo "<br>";
+	echo "Hello " .$nama;
+	?>
+</body>
+</html>
