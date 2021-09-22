5062c238d5fcd13ceea32f42dab3d96b87be336a
Adinda Nur Aulia Rizki <adindanar12@gmail.com>
2021-09-22 10:21:40 +0700

    Create array.php

---
+++ array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar Array PHP</title>
+</head>
+<body>
+	<?php 
+ 
+$biodata['nama'] = "Adinda Nur Aulia Rizki";
+$biodata['kelas'] = "MI20B";
+$biodata['alamat'] = "Tasikmalaya";
+$biodata['asal'] = "LP3I";
+ 
+
+echo $biodata['alamat'];                             
+ 
+?>
+</body> 
+</html>
