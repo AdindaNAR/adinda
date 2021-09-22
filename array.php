d1a64afd8ec86a4839d9da864db293d6338d8dc7
Adinda Nur Aulia Rizki <adindanar12@gmail.com>
2021-09-22 11:25:24 +0700

    Update array.php

--- array.php
+++ array.php
@@ -4,6 +4,7 @@
 	<title>Belajar Array PHP</title>
 </head>
 <body>
+<<<<<<< Updated upstream
 	<?php 
 		 
 		$biodata['nama'] = "Adinda Nur Aulia Rizki";
@@ -11,8 +12,30 @@
 		$biodata['alamat'] = "Tasikmalaya";
 		$biodata['asal'] = "LP3I";
 		 
+=======
+	<h1>My Biodata</h1>
 
-echo $biodata['alamat'];                             
+	<?php    
+	$biodata = array(
+	'nama' => "Nama : Adinda Nur Aulia Rizki",
+	'kelas' => "Kelas : MI20B",
+	'alamat' => "Alamat : Tasikmalaya",
+	'nohp' => "No Hp : 083121405709"
+	);
+	$matakuliah = array('Web Programming', 'English for Special Purpose','Java Programming','Network Operating System');
+ 
+>>>>>>> Stashed changes
+
+	echo $biodata['nama'];
+	echo "<br>";
+	echo $biodata['kelas'];
+	echo "<br>";
+	echo $biodata["alamat"];
+	echo "<br>";
+	echo $biodata["nohp"];
+	echo "<br>";
+	print_r($matakuliah);
+                     
  
 ?>
 </body> 
