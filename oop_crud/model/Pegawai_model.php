e99857ca380fd7a88be7f82de7ed3ff6b9386175
AdindaNAR <adindanar12@gmail.com>
2021-11-17 14:11:01 +0700

    Update Pegawai_model.php

--- oop_crud/model/Pegawai_model.php
+++ oop_crud/model/Pegawai_model.php
@@ -6,12 +6,74 @@ class Pegawai_model{
 	function __construct($db){ //fungsi function construct = autoload
 		$this->db = $db;
 	}
+
 	function tampil_data()
 	{
 		$row = $this->db->prepare("SELECT * FROM tbl_pegawai");
 		$row->execute();
 		return $hasil = $row->fetchAll();
 	}
+
+	function getData($id)
+	{
+		$row =$this->db->prepare("SELECT * FROM tbl_pegawai where id = $id");
+		$row->execute();
+		return $hasil = $row->fetch();
+	}
+
+	function getJenisData()
+	{
+		$row = $this->db->prepare("SELECT * FROM tbl_posisi_pegawai");
+		$row->execute();
+		return $hasil = $row->fetchAll();
+	}
+
+	function simpanData($data)
+	{
+		//buat array untuk isi values insert sumber kode
+		$rowsSQL = array();
+		//buat bind param Prepared Statement
+		$toBind = array();
+		//list nama kolom
+		$columnNames = array_keys($data[0]);
+		//looping untuk mengambil isi dari kolom / values
+		foreach($row as $columnNames => $columnValue){
+			$param = ":" . $columnName . $arrayIndex;
+			$params[] = $param;
+			$toBind[$param] - $columnValue; 
+		}
+		$rowsSQL[] = "(" . implode(",", $columnNames) . ") VALUES" . implode(",", $rowsSQL);
+		$row = $this->db->prepare($sql);
+		foreach ($toBind as $param => $val) {
+			$row -> bindValue($param, $val);
+		}
+	}
+
+		function updateData($data,$id)
+		{
+			$setPart = array();
+			foreach ($data as $key => $value)
+			{
+				$setPart[] = $key. "=:" .$key;
+			}
+			$sql = "UPDATE tbl_pegawai SET ".implode(', ', $setPart)."WHERE id = :id";
+			$row = $this->db->prepare($sql);
+			//Bind our values
+			$row->bindValue(':id',$id);//where
+			foreach ($data as $param => $val) 
+			{
+				$row ->bindValue($param, $val);
+			}
+			return $row->execute();
+		}
+		
+		function hapusData($id)
+		{
+			$sql = "DELETE FROM tbl_pegawai WHERE id = ?";
+			$row = $this->db->prepare($sql);
+			return $row -> execute(array($id));
+		}
+	
 }
 
 
